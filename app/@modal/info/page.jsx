6ee4942260f927233f9d365e6dabc71b3a32e22e
hotline404 <cosmoyj7733@gmail.com>
2025-09-03 10:47:04 +0900

    aesfasdf

--- app/@modal/info/page.jsx
+++ app/@modal/info/page.jsx
@@ -45,7 +45,7 @@ function Info() {
 
   return (
     <div className="fixed inset-0 bg-black/70 flex-col items-center justify-center z-50">
-      <div className="flex-col justify-center items-center">
+      <div className="flex justify-center items-center">
         <div className="flex justify-center mt-16 text-yellow-400">
           <img
             className="h-50 w-50 object-contain rounded-full"
