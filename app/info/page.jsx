fea37404269224df80b9e371972b5badd42190a3
hotline404 <cosmoyj7733@gmail.com>
2025-09-04 17:10:29 +0900

    tewst

--- app/info/page.jsx
+++ app/info/page.jsx
@@ -35,7 +35,7 @@ function Info() {
 
   return (
     <div className="fixed flex flex-col justify-center items-center inset-0 bg-black/70 z-50">
-      <div className="flex flex-col w-[50%] justify-center ">
+      <div className="flex w-[50%] justify-center ">
         <div className="flex mt-16 text-yellow-400 ">
           <img
             className="h-50 w-50 object-contain rounded-full"
