cb1b3032bc85479c0f79f4deeea3c23d25d19bd5
hotline404 <cosmoyj7733@gmail.com>
2025-09-04 11:57:22 +0900

    feat : link

--- components/info/ProjectList.jsx
+++ components/info/ProjectList.jsx
@@ -10,7 +10,7 @@ function ProjectList({ data }) {
     <ol className='flex-col text-yellow-400 justify-center mt-3'>
       /* projects */
       {state?.projects.map((item, index) => {
-        return <li className='text-yellow-400' id={Math.random()}><a href={`https://github.com/${item}`} target='_blank'>{`project ${index + 1} : ${item} 링크`}</a></li>
+        return <li className='text-yellow-400' id={Math.random()}><a href={`https://github.com/${item}`} target='_blank'>{`project link ${index + 1} : ${item}`}</a></li>
       })}
     </ol>
   )
