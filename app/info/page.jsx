c3e809e8557ad92f74054e217418e190bf1d96c6
hotline404 <cosmoyj7733@gmail.com>
2025-09-04 09:43:03 +0900

    fix : import

--- app/info/page.jsx
+++ app/info/page.jsx
@@ -1,20 +1,10 @@
-//step 1 rfce ✅
-//step 2 return box ✅
-//step 3 button back ✅
-//step 4 make components
-// step 4-1 common info (name, email, github)
-// step 4-2 skill img (list > img)
-// step 4-3 primary project list with link
-// step 4-4 sns link list with link
-// step 4-5 ...
-//step 5 import components ✅
-//step 6 style components
+
 import React from "react";
 import BackButton from "@/components/Button/BackButton";
-import CommonInfo from "../../../components/info/CommonInfo";
-import ProjectList from "../../../components/info/ProjectList";
-import SkillInfo from "../../../components/info/SkillInfo";
-import SnsList from "../../../components/info/SnsList";
+import CommonInfo from "@/components/info/CommonInfo";
+import ProjectList from "@/components/info/ProjectList";
+import SkillInfo from "@/components/info/SkillInfo";
+import SnsList from "@/components/info/SnsList";
 import { getInfo } from "@/API/service";
 
 export const dynamic = "force-dynamic";
