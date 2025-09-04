d0001a83e32ffe57dbd78ee654ec5464ff11367f
hotline404 <cosmoyj7733@gmail.com>
2025-09-04 12:33:49 +0900

    feat: adsf

--- app/@modal/default.jsx
+++ app/@modal/default.jsx
@@ -1,39 +1,9 @@
 // 루트 페이지 크기 및 페이지 색상
 import React from "react";
-import MainBox from "@/components/box/MainBox";
-import { mixtureStyle } from "@/components/style/mixture/mixture";
-import Header from "@/components/header/Header";
-import ListBox from "@/components/box/ListBox";
-import Provider from "@/store/Provider.jsx";
 
-require("dotenv").config();
-
-import { getNotion } from "@/API/service.js";
-
-//page 정보
-export const dynamic = "force-dynamic";
-
-async function run() {
-  // notion api 연결 및 디버깅
-  try {
-    const res = await getNotion();
-    return res;
-  } catch (error) {
-    console.error("get error", error);
-  }
-}
-
-function Home() {
+function Default() {
   const homePageStyle = mixtureStyle;
-  return (
-    <div className={homePageStyle.base_note}>
-      <Provider data={run()}/>
-      <MainBox BoxAttribute={homePageStyle.page}>
-        <Header />
-        <ListBox />
-      </MainBox>
-    </div>
-  );
+  return null;
 }
 
-export default Home;
+export default Default;
