e9f845fdab12afff478f29df2c4d1ace4fb81a35
hotline404 <cosmoyj7733@gmail.com>
2025-09-06 07:04:10 +0900

    feat: global

--- components/Dither.jsx
+++ components/Dither.jsx
@@ -6,7 +6,7 @@ import { EffectComposer, wrapEffect } from '@react-three/postprocessing';
 import { Effect } from 'postprocessing';
 import * as THREE from 'three';
 
-import './Dither.css';
+import './glogbals.css';
 
 const waveVertexShader = `
 precision highp float;
