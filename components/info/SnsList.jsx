b8a70f43f95b75a83db5f5e9b63c90be06035f05
hotline404 <cosmoyj7733@gmail.com>
2025-09-05 11:43:35 +0900

    feat : flex

--- components/info/SnsList.jsx
+++ components/info/SnsList.jsx
@@ -11,29 +11,33 @@ function SnsList({ data }) {
     if (url.includes("x.com")) {
       return (
         <img
-            src='https://img.shields.io/badge/-x-000000?logo=x&logoColor=white'
-            alt='x 로고'
-            className="mb-3 ml-3 w-18 h-8"
-          />
+          src="https://img.shields.io/badge/-x-000000?logo=x&logoColor=white"
+          alt="x 로고"
+          className="mb-3 ml-3 w-18 h-8"
+        />
       );
     } else if (url.includes("instagram.com")) {
-      return <img
-            src='https://img.shields.io/badge/-instagram-FF0069?logo=instagram&logoColor=white'
-            alt='instagram 로고'
-            className="mb-3 ml-3 w-26 h-8"
-          />;
+      return (
+        <img
+          src="https://img.shields.io/badge/-instagram-FF0069?logo=instagram&logoColor=white"
+          alt="instagram 로고"
+          className="mb-3 ml-3 w-26 h-8"
+        />
+      );
     }
   };
   return (
-    <div className="flex mt-3 basis-1/2 min-w-0 text-yellow-400">
+    <div className="flex flex-col mt-3 basis-1/2 min-w-0 text-yellow-400">
       <span className="flex-1">📬contacts</span>
+      <div className="flex">
         {state?.sns.map((item) => {
           return (
-              <a href={item} target="_blank">
-                {isValidSns(item)}
-              </a>
+            <a href={item} target="_blank">
+              {isValidSns(item)}
+            </a>
           );
         })}
+      </div>
     </div>
   );
 }
