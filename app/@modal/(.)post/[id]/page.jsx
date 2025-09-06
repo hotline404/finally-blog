79dd854cc6ac868053c0dbb21998d7dd0418994f
hotline404 <cosmoyj7733@gmail.com>
2025-09-06 09:21:49 +0900

    feat add bg

--- app/@modal/(.)post/[id]/page.jsx
+++ app/@modal/(.)post/[id]/page.jsx
@@ -22,8 +22,8 @@ function Post({ params }) {
   });
 
   return (
-    <div className="fixed inset-0 bg-white/50 flex items-center justify-center z-50">
-      <div className="resize bg-[#FAF4E6] p-6 rounded-xl outline-[#31302B] outline-[5px] w-[80%] h-[80%]">
+    <div className="fixed inset-0 bg-black/70 flex items-center justify-center z-50">
+      <div className="resize bg-[#EBEBEB] p-6 rounded-xl outline-[#31302B] outline-[5px] w-[80%] h-[80%]">
         <h2 className="italic">/* P O S T P A G E */</h2>
         <div className="h-[90%]">
           <Article data={result} />
