0866cbdb704b75e1a2ec5edc704d26e5eb3f9ebe
hotline404 <cosmoyj7733@gmail.com>
2025-09-10 16:15:48 +0900

    아 존나 답답하네 시발 진짜

--- components/common/Modal.jsx
+++ components/common/Modal.jsx
@@ -1,21 +1,20 @@
 "use client";
 import BackButton from "../Button/BackButton";
-import { useRouter } from "next/navigation";
+import {useRouter} from 'next/navigation'
+
 
 function Modal(props) {
   const router = useRouter();
 
   return (
-    <div
-      className="fixed inset-0 h-screen w-full z-50 flex justify-center items-center"
-      onClick={() => {
-        router.back();
-      }}
-    >
-      <div className="absolute inset-0 bg-black/70 h-screen w-full"></div>
+    <div className="fixed inset-0 h-screen w-full z-50 flex justify-center items-center">
+      <div
+        className="absolute inset-0 bg-black/70 h-screen w-full"
+        onClick={() => {router.back()}}
+      ></div>
       <div className="absolute flex justify-center inset-0 items-center">
         {props.children}
-      </div>
+        </div>
       <div className="fixed top-0 right-10">
         <BackButton>❌</BackButton>
       </div>
