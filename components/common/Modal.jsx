ffacb12df6bb8b4c88b96131a0986d44a51735e2
hotline404 <cosmoyj7733@gmail.com>
2025-09-10 16:39:12 +0900

    feat: add adf

--- components/common/Modal.jsx
+++ components/common/Modal.jsx
@@ -1,6 +1,7 @@
 "use client";
 import BackButton from "../Button/BackButton";
-import { useRouter } from "next/navigation";
+import {useRouter} from 'next/navigation'
+
 
 function Modal(props) {
   const router = useRouter();
@@ -8,14 +9,13 @@ function Modal(props) {
   return (
     <div className="fixed inset-0 h-screen w-full z-50 flex justify-center items-center">
       <div
-        className="absolute inset-0 bg-black/70 h-screen w-full"
-        onClick={() => {
-          router.back();
-        }}
+        className="absolute inset-0 bg-black/70 h-screen w-full pointer-events-auto"
+        onClick={() => {router.back()}}
       ></div>
-      <div className="absolute flex justify-center inset-0 items-center pointer-events-none">
-        <div className="absolute pointer-events-auto flex justify-center items-center inset-0">{props.children}</div>
-      </div>
+      <div className="absolute flex justify-center inset-0 items-center pointer-events-auto
+      ">
+        {props.children}
+        </div>
       <div className="fixed top-0 right-10">
         <BackButton>❌</BackButton>
       </div>
