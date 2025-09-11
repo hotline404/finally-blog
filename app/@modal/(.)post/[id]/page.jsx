aa477f9844589c4d0308d5f32d167ac99efd0345
hotline404 <cosmoyj7733@gmail.com>
2025-09-11 18:28:22 +0900

    featadsfasdf

--- app/@modal/(.)post/[id]/page.jsx
+++ app/@modal/(.)post/[id]/page.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { getPost } from "@/API/service";
-import BackButton from "@/components/Button/BackButton";
 import Article from "@/components/common/Article";
 import Modal from "@/components/common/Modal";
+import { fetchDataBase } from "@/API/service";
 
 export const dynamic = "force-dynamic";
 export const maxDuration = 60;
@@ -11,7 +11,8 @@ function Post({ params }) {
   async function tryGet(id) {
     try {
       const res = await getPost(id);
-      console.log("* r e s   i n   p o s t   m o d a l *", res);
+      const test = await fetchDataBase(id);
+      console.log("* r e s   i n   p o s t   m o d a l *", test);
       return res;
     } catch (error) {
       console.error("e r r o r   i n   P o s t   m o d a l", error);
