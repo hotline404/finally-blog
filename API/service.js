25e0a93d6ff6523cad480e26b3952d02c86b32d6
hotline404 <cosmoyj7733@gmail.com>
2025-09-11 16:50:26 +0900

    feat: ???

--- API/service.js
+++ API/service.js
@@ -90,7 +90,7 @@ exports.getInfo = async function () {
 
 exports.fetchDataBase = async function () {
 
-  const res = await fetch(`https://api.notion.com/v1/data_sources/${notion_DB}/query`, postDBOptions)
+  const res = await fetch(`https://api.notion.com/v1/databases/${notion_DB}/query`, postDBOptions)
   .then(res => res.json())
   .then(res => console.log("test res in service", res))
   .catch(err => console.error(err));
