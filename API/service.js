a3841c737760c05add20e42c7fc1c186c0e18c3f
hotline404 <cosmoyj7733@gmail.com>
2025-09-11 21:12:56 +0900

    feat: ad dadf

--- API/service.js
+++ API/service.js
@@ -94,12 +94,12 @@ exports.fetchDataBase = async function () {
     })
     .catch((err) => console.error(err));
 
-  const post = results.filter(page => page.properties['상태'] === "공개").map((page) => {
+  const post = results.map((page) => {
     return {
       id: page.id,
       title: page.properties.Name.title[0].text.content,
       date: page.properties.date,
-      allProperties: page.properties,
+      allProperties: page.properties['상태'],
       icon: page.icon.emoji,
     };
   });
