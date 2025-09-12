f3c1babf1f83cedbe25e26390939b07d7e62fa42
hotline404 <cosmoyj7733@gmail.com>
2025-09-12 20:47:49 +0900

    hotfix

--- components/style/mixture/mixture.js
+++ components/style/mixture/mixture.js
@@ -15,6 +15,6 @@ export const mixtureStyle = {
   header: `${position.header} ${size.height.header} ${size.width.header} ${color.background.header} ${outline.header} ${color.outline.base_note} ${hover.size.header} ${transition.header} ${visible.input} drop-shadow-xl/50`,
   titleBox: `${position.titleBox} ${size.width.titleBox} ${size.height.titleBox} ${size.font.title} ${color.fontColor.base_note} cursor-pointer select-none`,
   searchBox: `${size.width.searchBox} ${size.height.searchBox} ${position.searchBox} ${outline.searchBox} `,
-  postListBox: `${size.width.postListBox} ${size.height.postListBox} ${size.font.postListBox} ${position.postListBox} ${outline.postListBox} ${overFlow.scrolly} ${overFlow.webkit_track_hide}` ,
-  postList: `${size.width.postList} ${size.height.postList} ${position.postList} ${outline.postList} ${shadow.postList} ${size.font.postList} ${hover.point.list} ${color.fontColor.base_note}`,
+  postListBox: `${size.width.postListBox} ${size.height.postListBox} ${size.font.postListBox} ${position.postListBox} ${outline.postListBox} ` ,
+  postList: `${size.width.postList} ${size.height.postList} ${position.postList} ${outline.postList} ${shadow.postList} ${size.font.postList} ${hover.point.list} ${color.fontColor.base_note} ${overFlow.scrolly} ${overFlow.webkit_track_hide}`,
 };
